3202c4679b5fae8e3a7380565393fe82370d9b48
JNKT215 <planetes215@gmail.com>
2024-12-30 15:31:12 +0900

    update sh scri@t

--- sh/tuning.sh
+++ sh/tuning.sh
@@ -1,80 +1,54 @@
 #tuning (normal)
 IFS_BACKUP=$IFS
 IFS=$'\n'
-
 dataset=$1
-
+gpu_id=$2
 if [ $dataset = "DBLP" ]; then
     num_hop=2
     num_label_hops=4
     submetapath_hops=2
+    neighbor_aggr_calc=basic
+    neighbor_aggr_mode=transformer
 elif [ $dataset = "IMDB" ]; then
     num_hop=4
     num_label_hops=4
     submetapath_hops=2
-    calc_type=linear
-    neighbor_aggr_mode=all
-    neighbor_encoder=mean
-    sampling_limit=10
-    submetapath_feature_weight=0.2
-
-
-elif [ $dataset = "ACM" ]; then
+    neighbor_aggr_calc=basic
+    neighbor_aggr_mode=transformer
+    elif [ $dataset = "ACM" ]; then
     num_hop=4
     num_label_hops=4
     submetapath_hops=2
+    neighbor_aggr_calc=basic
+    neighbor_aggr_mode=transformer
 fi
-
-echo $num_hop
-echo $num_label_hops
-echo $submetapath_hops
-echo $calc_type
-echo $neighbor_aggr_mode
-echo $neighbor_encoder
-echo $sampling_limit
-echo $submetapath_feature_weight
-
-
-
+echo gpu_id=$gpu_id
+echo num_hop=$num_hop
+echo num_label_hops=$num_label_hops
+echo submetapath_hops=$submetapath_hops
+echo neighbor_aggr_calc=$neighbor_aggr_calc
+echo neighbor_aggr_mode=$neighbor_aggr_mode
 ary=("
      python3 hgb/train.py -m 'key=SeHGNNver2_$dataset' \
-     'experiment_name=$dataset tuning' \
-     'SeHGNNver2_$dataset.dropout=choice(0, 0.2, 0.4, 0.5, 0.6, 0.8)' \
-     'SeHGNNver2_$dataset.num_hop=$num_hop' \
-     'SeHGNNver2_$dataset.num_label_hops=$num_label_hops' \
+     'experiment_name=tuning' \
      'SeHGNNver2_$dataset.label_feats=True' \
-     'SeHGNNver2_$dataset.residual=True' \
      'SeHGNNver2_$dataset.amp=True' \
-     'SeHGNNver2_$dataset.epochs=100' \
+     'SeHGNNver2_$dataset.epochs=50' \
      'SeHGNNver2_$dataset.run=5' \
      'SeHGNNver2_$dataset.embed_size=choice(256, 512)' \
-     'SeHGNNver2_$dataset.input_drop=choice(0, 0.2, 0.4, 0.6, 0.8)' \
-     'SeHGNNver2_$dataset.n_fp_layers=choice(2, 3, 4)' \
+     'SeHGNNver2_$dataset.lr=choice(1e-3, 1e-2, 5e-3, 5e-2)' \
+     'SeHGNNver2_$dataset.weight_decay=choice(0, 1e-4,  5e-4, 1e-3)' \
+     'SeHGNNver2_$dataset.dropout=choice(0, 0.2, 0.4, 0.5)' \
+     'SeHGNNver2_$dataset.input_drop=choice(0, 0.2, 0.4, 0.5)' \
      'SeHGNNver2_$dataset.n_task_layers=choice(1, 2, 3, 4)' \
-     'SeHGNNver2_$dataset.act=choice(none, relu, leaky_relu, sigmoid)' \
+     'SeHGNNver2_$dataset.gpu_id=$gpu_id' \
+     'SeHGNNver2_$dataset.num_hop=$num_hop' \
+     'SeHGNNver2_$dataset.num_label_hops=$num_label_hops' \
      'SeHGNNver2_$dataset.submetapath_hops=$submetapath_hops' \
-     'SeHGNNver2_$dataset.calc_type=$calc_type' \
+     'SeHGNNver2_$dataset.neighbor_aggr_calc=$neighbor_aggr_calc' \
      'SeHGNNver2_$dataset.neighbor_aggr_mode=$neighbor_aggr_mode'\
-     'SeHGNNver2_$dataset.neighbor_encoder=$neighbor_encoder' \
-     'SeHGNNver2_$dataset.sampling_limit=choice(10, 15)' \
-     'SeHGNNver2_$dataset.submetapath_feature_weight=$submetapath_feature_weight' \
      ")
-
-
-
-
-#  'SeHGNNver2_$dataset.lr=choice(0.01 ,5e-06 ,5e-05 ,0.0005 ,0.005 ,0.05)' \
-#  'SeHGNNver2_$dataset.weight_decay=choice(2e-06 ,2e-05 ,2e-04 ,0.002 ,5e-06 ,5e-05 ,5e-04 ,0.005)' \
-
 for STR in ${ary[@]}
 do
     eval "${STR}"
 done
-
-
-# source tuning.sh DBLP
-# source tuning.sh ACM
-# source tuning.sh IMDB
- 
-
- 
